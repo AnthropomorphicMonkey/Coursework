16c505d1bb2671d718c2e9f6258afe3a9bf5f7bb
Joe <30624504+Joe-Down@users.noreply.github.com>
2019-03-12 00:17:19 +0000

    Error patching

--- testing/sandbox.py
+++ testing/sandbox.py
@@ -1,7 +1,2 @@
-import datetime
-
-date = datetime.date(7809, 3, 5)
-print(date)
-due_year_month_day: list = str(date).split('-')
-string_due_date: str = "{}-{}-{}".format(due_year_month_day[0], int(due_year_month_day[1]), due_year_month_day[2])
-print(string_due_date)
+from PyQt5 import QtCore
+print(QtCore.PYQT_VERSION_STR)
