c61b9a57abcb81b9277dcba28cabf2f5507e22ce
Joe <30624504+Joe-Down@users.noreply.github.com>
2019-03-30 21:20:10 +0000

    Comments

--- maths_scripts/calculus.py
+++ maths_scripts/calculus.py
@@ -3,45 +3,65 @@ import sympy
 
 class IntegralApproximationBase:
     def __init__(self, number_of_strips: int, upper_limit: float, lower_limit: float, function):
+        # Errors if invalid number of strips entered
+        if number_of_strips < 1:
+            raise Exception('Number of strips must be greater than 0')
+        # Stores all constant values related to the range within which the integral is to be calculated, what function
+        # is being integrated and how many strips are to be used (accuracy level of approximation)
         self.number_of_strips: int = number_of_strips
-        self.strip_width: float = (upper_limit - lower_limit) / number_of_strips
         self.upper_limit: float = upper_limit
         self.lower_limit: float = lower_limit
         self.function = function
-        self.y_list: list = []
-        self.get_y_values()
+        # Calculates strip width as the range of the integral divided by the desired number of strips
+        self.strip_width: float = (upper_limit - lower_limit) / number_of_strips
+        # Finds and stores the y values at the boundary between each strip
+        self.y_list: list = self.get_y_values()
 
-    def get_y_values(self):
-        y_counter = 0
+    def get_y_values(self) -> list:
+        # Variable to keep track of number of y values calculated
+        y_counter: int = 0
+        # List to store all y values
+        y_values: list = []
+        # Until each y value has been found, calculates the y for value for each x value from the lower limit to the
+        # upper limit at intervals of the strip width and stores within the y_values attribute
         while y_counter <= self.number_of_strips:
             x_value: float = self.lower_limit + (self.strip_width * y_counter)
             x = sympy.symbols('x')
-            self.y_list.append(self.function.subs(x, x_value))
+            y_values.append(self.function.subs(x, x_value))
             y_counter += 1
+        # Returns list of y values
+        return y_values
 
 
 class TrapeziumRule(IntegralApproximationBase):
     def __init__(self, number_of_strips: int, upper_limit: float, lower_limit: float, function):
-        if number_of_strips < 1:
-            raise Exception('Number of strips must be greater than 0')
+        # Initialises child class with no changes to parent class
         super().__init__(number_of_strips, upper_limit, lower_limit, function)
 
     def integral(self):
+        # Approximates integral using equation defined in design. Y sum stores the sum of all the y values, with all
+        # but the first and last being added twice as they are both the end of one trapezium and start of the next
         y_sum: float = self.y_list[0] + self.y_list[-1]
         counter: int = 1
         for y in self.y_list[1:-1]:
             y_sum += y * 2
             counter += 1
+        # Multiplies y sum by half the strip width to get the approximate integral and returns the value
         return float((self.strip_width / 2) * y_sum)
 
 
 class SimpsonsRule(IntegralApproximationBase):
     def __init__(self, number_of_strips: int, upper_limit: float, lower_limit: float, function):
+        # Verifies that strip count is even as required(see design) and errors if not (validation should occur before
+        # using class), then initialises class identically to parent class
         if number_of_strips % 2 != 0:
             raise Exception('Number of strips must be even')
         super().__init__(number_of_strips, upper_limit, lower_limit, function)
 
     def integral(self):
+        # Approximates integral using equation defined in design. y sum stores the sum of all y values, with the first
+        # and last value being added once, and all other values from the second y value through to the penultimate
+        # alternating between being added four times and twice respectively
         y_sum: float = self.y_list[0] + self.y_list[-1]
         counter: int = 1
         for y in self.y_list[1:-1]:
@@ -50,6 +70,7 @@ class SimpsonsRule(IntegralApproximationBase):
             else:
                 y_sum += y * 4
             counter += 1
+        # Multiplies y sum by 1/3 the strip width to get the approximate integral and returns the value
         return float((self.strip_width / 3) * y_sum)
 
 
@@ -61,14 +82,5 @@ class DefiniteIntegral(SimpsonsRule):
 
 if __name__ == '__main__':
     x1 = sympy.symbols('x')
-    fn = x1 ** 4 + 5 * x1 ** 2 + 4 * x1 + 9
-    upper = 100
-    lower = 1
-    print("∫", fn, "dx between x =", lower, "and x =", upper)
-    strips = 1000
-    trapezium = TrapeziumRule(strips, upper, lower, fn)
-    print(trapezium.integral())
-    simpson = SimpsonsRule(strips, upper, lower, fn)
-    print(simpson.integral())
-    numerical = DefiniteIntegral(upper, lower, fn)
-    print(numerical.integral())
+    f = (2 * x1 + 1) ** 0.5
+    instance = SimpsonsRule(5, 1, 0, f)
--- maths_scripts/series.py
+++ maths_scripts/series.py
@@ -2,44 +2,57 @@ import sympy
 import maths_scripts
 
 
+# Class to generate a taylor series and approximate values fora given function and centre
 class Taylor:
     def __init__(self, function: sympy.add.Add, centre: int):
+        # Sets up x as a variable in SymPy
         x = sympy.symbols('x')
+        # Defines constants containing the function to be expanded and the centre
         self.function = function
         self.centre = centre
+        # Outputs an error if a function is entered with an invalid centre (function is undefined at centre)
         if function.subs(x, self.centre) == sympy.zoo:
             raise Exception("Invalid centre")
 
     def equation_in_x(self, precision: int) -> sympy.add.Add:
+        # Sets up x as a variable in SymPy
         x = sympy.symbols('x')
+        # Initialises variable to hold final function as nothing
         final_equation: sympy.add.Add = sympy.add.Add(0)
+        # Initialises variable to hold the next derivative to be used during expansion as the value of the initial
+        # function
         function: sympy.add.Add = self.function
+        # Loops through for the number of terms entered as the precision and generates next term in the Taylor series
+        # on each loop
         for n in range(0, precision):
+            # Calculates the value of the current function at the centre of expansion
             f: float = function.subs(x, self.centre)
+            # Uses formula (see design section) to generate the next term in x in the Taylor series and add to end of
+            # current full expansion
             final_equation += (f / maths_scripts.integer_factorial(n)) * ((x - self.centre) ** n)
+            # Replaces current function with the derivative of the current function
             function = sympy.diff(function, x)
+        # Returns full expansion up to number of terms specified
         return final_equation
 
     def evaluate(self, value: float, precision: int) -> float:
+        # Sets up x as a variable in SymPy
         x = sympy.symbols('x')
+        # Generates Taylor series to given precision and substitutes value of x with given value to evaluate function
         function = self.equation_in_x(precision)
+        # Returns final value
         return function.subs(x, value)
 
 
 class Maclaurin(Taylor):
     def __init__(self, function: sympy.add.Add):
+        # Inherits all code from Taylor series class while as Maclaurin series is simply a special case of it with
+        # centre of expansion 0. Centre of expansion is therefore not required to be passed when declaring instance of
+        # class as it can always be passed to parent class as 0
         super().__init__(function, 0)
 
 
 if __name__ == '__main__':
-    x1: sympy.add.Add = sympy.symbols('x')
-    function1 = sympy.ln(8)
-    centre1: int = 6
-    precision1: int = 100
-    value1: float = 1
-    series: Taylor = Taylor(function1, centre1)
-    print(series.equation_in_x(precision1))
-    print(float(series.evaluate(value1, precision1)))
-    series: Maclaurin = Maclaurin(function1)
-    print(series.equation_in_x(precision1))
-    print(float(series.evaluate(value1, precision1)))
+    x = sympy.symbols('x')
+    f = Taylor(sympy.ln(1 + x), 0)
+    print(float(f.evaluate(4, 500)))
